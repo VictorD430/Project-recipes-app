bd93726cb5de377de675361ceb09401d8cb00dba
Gilliady <gilliadyazevedo@gmail.com>
2023-06-05 18:02:55 -0300

    finalizado requisito 7 iniciado 8 e 9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
-import Recipes from './pages/Recipes';
+import Home from './pages/Home';
+import Recipe from './pages/Recipe';
+import InProgress from './pages/InProgress';
+import Profile from './pages/Profile';
+import Done from './pages/Done';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
@@ -19,7 +24,19 @@ function App() {
       </object>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/meals" component={ Recipes } />
+
+        <Route exact path="/meals" component={ Home } />
+        <Route exact path="/drinks" component={ Home } />
+
+        <Route exact path="/meals/:recipeId" component={ Recipe } />
+        <Route exact path="/drinks/:recipeId" component={ Recipe } />
+
+        <Route exact path="/meals/:recipeId/in-progress" component={ InProgress } />
+        <Route exact path="/drinks/:recipeId/in-progress" component={ InProgress } />
+
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ Done } />
+        <Route exact path="/favorite-recipes" component={ Favorites } />
       </Switch>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import ProfileIcon from './ProfileIcon';
+import Search from './Search';
 
 export default function Header() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/meals" render={ () => <div>Oi</div> } />
-      </Switch>
-    </div>
+    <header>
+      <nav>
+        <ProfileIcon />
+        <Switch>
+          <Route
+            exact
+            path="/meals"
+            render={ () => (
+              <>
+                <Search />
+                <h2 data-testid="page-title">Meals</h2>
+              </>
+            ) }
+          />
+          <Route
+            exact
+            path="/drinks"
+            render={ () => (
+              <>
+                <Search />
+                <h2 data-testid="page-title">Drinks</h2>
+              </>
+            ) }
+          />
+          <Route
+            exact
+            path="/profile"
+            render={ () => (
+              <h2 data-testid="page-title">Profile</h2>
+            ) }
+          />
+          <Route
+            exact
+            path="/done-recipes"
+            render={ () => (
+              <h2 data-testid="page-title">Done Recipes</h2>
+            ) }
+          />
+          <Route
+            exact
+            path="/favorite-recipes"
+            render={ () => (
+              <h2 data-testid="page-title">Favorite Recipes</h2>
+            ) }
+          />
+        </Switch>
+      </nav>
+    </header>
   );
 }
