d1e98d7017317e4d2323a1b9225c1e96ba953565
Gilliady <gilliadyazevedo@gmail.com>
2023-06-07 17:08:43 -0300

    começando requisitos 19-23

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from '../components/Header';
+import { getDrinksAPI, getMealsAPI } from '../services/fetchAPI';
 
 export default function Recipes() {
   const { recipes: { meals, drinks } } = useSelector((state) => state);
+  const [categories, setCategories] = useState([]);
+  const history = useHistory();
+  useEffect(() => {
+    const makeFetch = async () => {
+      if (history.location.pathname === '/meals') {
+        const apiCategories = await getMealsAPI('list.php?c=list');
+        setCategories(apiCategories);
+        return;
+      }
+      const apiCategories = await getDrinksAPI('list.php?c=list');
+      setCategories(apiCategories);
+    };
+    makeFetch();
+  }, [setCategories, history]);
+  console.log(categories);
   return (
     <>
       <Header />
--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,7 +1,7 @@
 export const getMealsAPI = async (endpoint) => {
   const URL = `https://www.themealdb.com/api/json/v1/1/${endpoint}`;
   const response = await fetch(URL);
-  console.log(response);
+  console.log(URL);
   const data = await response.json();
   return data;
 };
