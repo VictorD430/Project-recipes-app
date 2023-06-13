3c2fd283f823d12ae38462c3261eefe929151e0a
Raissa Gonçalves de Oliveira <raissa@MacBook-Pro-de-Raissa.local>
2023-06-13 19:04:02 -0300

    finalizado requisito 38

--- src/pages/RecipeInProgres.jsx
+++ src/pages/RecipeInProgres.jsx
@@ -8,7 +8,15 @@ export default function InProgress() {
   const type = locationPathName[1];
   const id = locationPathName[2];
   const [recipe, setRecipe] = useState({ data: [], type: 'nao_definido' });
-  const nameOfPropSRC = type === 'meals' ? recipe.data.strMealThumb : recipe.data.strDrinkThumb;
+  const nameOfPropSRC = type === 'meals'
+    ? recipe.data.strMealThumb : recipe.data.strDrinkThumb;
+
+  const ingredients = Object.entries(recipe.data)
+    .filter((i) => i[0].includes('strIngredient') && (i[1] !== '' && i[1] !== null));
+  const measures = { ...ingredients };
+  ingredients.forEach((item, index) => {
+    measures[index] = recipe.data[`strMeasure${index + 1}`] || ' ';
+  });
 
   useEffect(() => {
     const getRecipeInfo = async () => {
@@ -30,7 +38,6 @@ export default function InProgress() {
       InProgress -
       {' '}
       {type}
-
       <img src={ nameOfPropSRC } alt="recipe-img" data-testid="recipe-photo" />
       <h1 data-testid="recipe-title">{recipe.data.strArea}</h1>
       <button data-testid="share-btn"> compartilhar</button>
@@ -38,6 +45,16 @@ export default function InProgress() {
       <p data-testid="recipe-category">{recipe.data.strCategory}</p>
       <p data-testid="instructions">{recipe.data.strInstructions}</p>
       <button data-testid="finish-recipe-btn">Finish</button>
+      <h2>Ingredients</h2>
+      {ingredients.map((i, index) => (
+        <label key={ index } data-testid={ `${index}-ingredient-step` }>
+          <input
+            type="checkbox"
+          />
+          {
+            `${i[1]} - ${measures[index]}`
+          }
+        </label>))}
 
     </div>
   );
